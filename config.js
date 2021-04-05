9f182797f1a4c00a8fa5d828ec438d680519bf88
chunlee8189 <60812666+chunlee8189@users.noreply.github.com>
2021-04-06 00:43:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur1259075-b223b994da15df15cd0e8765'
   ],
 
   // 是否显示监测站点的链接
